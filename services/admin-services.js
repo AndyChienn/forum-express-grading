d83be33d5bc96eb9edd3e228d87e82568cadf21b
AndyChienn <andy2002chien@hotmail.com.tw>
2022-07-14 09:06:45 +0800

    feat: add api error handler & delete restaurant api

--- services/admin-services.js
+++ services/admin-services.js
@@ -9,6 +9,15 @@ const adminServices = { // 修改這裡
     })
       .then(restaurants => cb(null, { restaurants }))
       .catch(err => cb(err))
+  },
+  deleteRestaurant: (req, cb) => {
+    Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
+      })
+      .then(deletedRestaurant => cb(null, { restaurant: deletedRestaurant }))
+      .catch(err => cb(err))
   }
 }
 module.exports = adminServices
