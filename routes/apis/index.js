e6905769ea51a8c34d678bee02d6e418468146fd
AndyChienn <andy2002chien@hotmail.com.tw>
2022-07-14 18:50:09 +0800

    feat: modify api unauthorize response

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -7,9 +7,11 @@ const userController = require('../../controllers/apis/user-controller')
 const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 
+router.post('/signup', userController.signUp)
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+
 router.use('/admin', authenticated, authenticatedAdmin, admin)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 
-router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.use('/', apiErrorHandler)
 module.exports = router
